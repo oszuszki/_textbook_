6abad83cbf3bfc8efc6b48af66104970ecc08a7d
oszuszki <50295110+oszuszki@users.noreply.github.com>
2019-05-10 06:44:22 +0200

    Add files via upload

---
+++ _files_/_caesar_/dharomszog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>		// malloc()
+
+int main(void)
+{
+	const int db = 5;
+
+	double **hm;
+	printf("Mutato cime: %p\n", &hm);
+
+	hm = (double **)malloc(db * sizeof(double));
+	printf("Sorok tombjenek cime: %p\n", hm);
+
+	for (int i = 0; i < db; i++)
+		hm[i] = (double *)malloc(db * sizeof(double));
+	printf("Elso sor cime: %p\n", hm[0]);
+
+	for (int i = 0; i < db; i++) {
+		printf("|");
+		for (int j = 0; j < db; j++) {
+			hm[i][j] = (i + 1) / (j + 1);
+			printf("%.2f  ", hm[i][j]);
+		}
+		printf("|\n");
+	}
+
+	return 0;
+}
